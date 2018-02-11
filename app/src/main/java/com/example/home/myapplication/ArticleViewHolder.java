26e9ef48536e52dc860ee00e5926ebbf975014e3
home <brnli7@gmail.com>
2018-02-10 21:56:15 -0500

    Added ability to open a Reddit URL

--- app/src/main/java/com/example/home/myapplication/ArticleViewHolder.java
+++ app/src/main/java/com/example/home/myapplication/ArticleViewHolder.java
@@ -1,5 +1,7 @@
 package com.example.home.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,8 +25,15 @@ class ArticleViewHolder extends RecyclerView.ViewHolder {
         this.thumbnail = itemView.findViewById(R.id.thumbnail);
     }
 
-    public void populate(RedditNewsDataResponse redditNewsDataResponse) {
+    public void populate(final RedditNewsDataResponse redditNewsDataResponse) {
         this.title.setText(redditNewsDataResponse.getTitle());
         Picasso.with(itemView.getContext()).load(redditNewsDataResponse.getThumbnail()).into(thumbnail);
+        this.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(redditNewsDataResponse.getUrl()));
+                itemView.getContext().startActivity(browserIntent);
+            }
+        });
     }
 }
